a544941c1ab1f1629789f3fd3c937958601fd669
Chaker Necibi <154261868+chakerncb@users.noreply.github.com>
2025-05-02 12:46:32 +0100

    feat: enhance authentication by adding login functionality and user DTOs

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Dtos.Account;
+using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -14,8 +16,52 @@ namespace api.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<User> _userManager;
-        public AccountController(UserManager<User> userManager){
+        private readonly ITokenService _tokenService;
+        private readonly SignInManager<User> _signInManager;
+        public AccountController(UserManager<User> userManager, ITokenService tokenService, SignInManager<User> signInManager){
             _userManager = userManager;
+            _tokenService = tokenService;
+            _signInManager = signInManager;	
+        }
+
+        [HttpPost("login")]
+
+
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            try {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+                if (user == null)
+                {
+                    return Unauthorized("Invalid username or password.");
+                }
+
+                var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+                if (result.Succeeded)
+                {
+                    return Ok(new NewUserDto
+                    {
+                        UserName = user.UserName,
+                        Email = user.Email,
+                        Token = _tokenService.CreateToken(user)
+                    });
+                }
+                else
+                {
+                    return Unauthorized("Invalid username or password.");
+                }
+
+            } catch (Exception e){
+                
+                return StatusCode(500,e);
+            }
         }
 
 
@@ -49,7 +95,14 @@ namespace api.Controllers
 
                     if (roleResult.Succeeded)
                     {
-                        return Ok("User created successfully.");
+                        return Ok(
+                            new NewUserDto
+                            {
+                                UserName = user.UserName,
+                                Email = user.Email,
+                                Token = _tokenService.CreateToken(user)
+                            }
+                        );
                     }
                     else
                     {
